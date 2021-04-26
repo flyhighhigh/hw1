cd49eb29045ba0a4d7acba4d15ab7f5733638d48
flyhighhigh <galenliu910322@gmail.com>
2021-04-27 06:37:17 +0800

    finish radix str

--- main.h
+++ main.h
@@ -15,7 +15,8 @@ char *data_str[data_cnt+5];
 char *temp_str[data_cnt+5];
 int data_int[data_cnt+5];
 int temp_int[data_cnt+5];
-int radix_list[10][data_cnt];
+int radix_list_int[10][data_cnt];
+char *radix_list_str[52][data_cnt];
 
 int firstpage();
 void start();
@@ -36,4 +37,6 @@ void mergearray_int(int a,int a_end,int b,int b_end);
 void rec_merge_str(int left,int right);
 void mergearray_str(int a,int a_end,int b,int b_end);
 //radix
+void radixsort();
 void radix_int();
+void radix_str();
--- radixsort.c
+++ radixsort.c
@@ -1,7 +1,7 @@
 void radixsort(){
     
     if(option==STR)
-        ;//quick_str(0,data_cnt-1);
+        radix_str();
     
     else
         radix_int();
@@ -9,27 +9,40 @@ void radixsort(){
 }
 
 void radix_int(){
-    int n=1;
-    int zerocount=1;
+    long n=1;
+    int len=10;
 
-    while(zerocount!=data_cnt){
+    while(len--){
         int count[10]={0};
-        for(int i=0;i<10;i++){//¨C­Ó¼Æ¦r0~9
-            for(int j=0;j<data_cnt;j++){
-                if(data_int[j]/n%10==i){
-                    radix_list[i][count[i]++]=data_int[j];
-                }
-            }
+        for(int j=0;j<data_cnt;j++){
+            int i=data_int[j]/n%10;
+            radix_list_int[i][count[i]++]=data_int[j];
         }
         int idx=0;
         for(int i=0;i<10;i++){
             for(int j=0;j<count[i];j++){
-                data_int[idx++]=radix_list[i][j];
+                data_int[idx++]=radix_list_int[i][j];
             }
         }
         n*=10;
-        zerocount=count[0];
     }
-    
+    return;
+}
+
+void radix_str(){
+
+    for(int k=99;k>=0;k--){
+        int count[52]={0};
+        for(int j=0;j<data_cnt;j++){
+            int c=isupper(data_str[j][k])?data_str[j][k]-'A':data_str[j][k]-'a'+26;
+            radix_list_str[c][count[c]++]=data_str[j];
+        }
+        int idx=0;
+        for(int i=0;i<52;i++){
+            for(int j=0;j<count[i];j++){
+                data_str[idx++]=radix_list_str[i][j];
+            }
+        }
+    }
     return;
 }
